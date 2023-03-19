e249fcde9290e3114a5c04fde4fa17a7e8b690d6
Wild-Bill <spamy4@mail.ru>
2023-03-19 22:18:14 +0300

    add lamda

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
+  int maxWords = 2;
     public static void main(String[] args) {
       List<Person>people = new ArrayList<>();
       people.add(new Person("Ivan", "Vasilev Novicov", 3));
@@ -10,8 +12,13 @@ public class Main {
         people.add(new Person("Tanay", "Maximova-Vasilevskay", 1));
         people.add(new Person("Olya", "Petrova", 4));
         people.add(new Person("Alex", "Tern", 3));
+
+
+     // Comparator<Person> comparator = new (Person o1, Person o2) -> {
+     // }
+
         System.out.println(people);
-        Collections.sort(people);
+        Collections.sort(people, comparator);
         System.out.println(people);
     }
 
