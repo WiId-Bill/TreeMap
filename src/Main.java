94ea293e29b7bf36beb8401a0e9d5b453eb6a5e1
Wild-Bill <spamy4@mail.ru>
2023-03-21 02:10:11 +0300

    add eraser and removeif

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
   private static final int  maxWords = 3;
     public static void main(String[] args) {
       List<Person>people = new ArrayList<>();
-      people.add(new Person("Ivan", "Vasilev Novicov", 3));
+      people.add(new Person("Ivan", "Vasilev Novicov", 73));
         people.add(new Person("Petya", "Ivanov", 5));
         people.add(new Person("Tanay", "Maximova-Vasilevskay", 1));
-        people.add(new Person("Olya", "Petrova", 4));
-        people.add(new Person("Alex", "Tern gffg gbfdgfg gfgfd", 3));
+        people.add(new Person("Olya", "Petrova", 24));
+        people.add(new Person("Alex", "Tern gffg gbfdgfg gfgfd", 23));
+      people.add(new Person("Max", " Bean Beer", 3));
+      people.add(new Person("Mikhail", "Pupkin-Nosov", 33));
 
 
       Comparator<Person> comparator = (Person o1, Person o2) -> {
@@ -30,6 +32,7 @@ public class Main {
       };
 
         System.out.println(people);
+      people.removeIf(person -> person.getAge()<18);
         Collections.sort(people, comparator);
         System.out.println(people);
     }
